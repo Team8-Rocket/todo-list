9907b8be8313bfec07d79dc31741003e0e0c095e
lumpenop <swk5373@gmail.com>
2022-05-05 14:54:51 +0900

    Feat: Add Search function assets/svgs 에 magify.svg 로 돋보기 아이콘 추가, assets/svgs/index.js 에 Magnify export 추가 TodoList/index.js 에 Magnify 를 이용해  검색 기능 추가, TodoList.module.scss에 스타일 추가 -> Magnify 아이콘 onClick 시 input text 창이 열리고 onChange 를 이용해 검색/디바운싱 구현

--- src/routes/TodoList/index.js
+++ src/routes/TodoList/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import styles from './TodoList.module.scss'
-import { CheckIcon } from '../../assets/svgs'
+import { CheckIcon, Magnify } from '../../assets/svgs'
+import {cx} from '../../styles'
 
 const INIT_TODO = [
   {
@@ -24,9 +25,51 @@ const INIT_TODO = [
 
 function TodoList() {
   const [todoList, setTodoList] = useState(INIT_TODO)
+  const [isSearchClicked, setIsSearchClicked] = useState(false)
+  const [searchValue, setSearchValue] = useState('')
+  const [debounceTimer, setDebounceTimer] = useState(0)
 
-  const handleAddClick = (e) => {
-    // console.log('handleAddClick')
+
+  const search = (value) =>{
+
+    const newTodo = INIT_TODO.filter(ele => {
+      const title = ele.title.replace(' ','').toLowerCase()
+      const newValue = value.replace(' ','')[0].toLowerCase()
+      return (
+        title.includes(newValue)
+      )
+    })
+    setTodoList(()=> newTodo)
+  }
+  
+  const deboucingSearch = (text) => {
+    if(debounceTimer){
+      clearTimeout(debounceTimer)
+    }
+    const newTimer = setTimeout(async () =>{
+      await search(text)
+    }, 250)
+    setDebounceTimer(newTimer)
+  }
+
+  const handleSearchClick = () => {
+    setSearchValue(()=>'')
+    setIsSearchClicked(prev => !prev)
+    if(isSearchClicked){
+      setTodoList(()=>INIT_TODO)
+    }
+  }
+
+  const handleSearchChange = (event) => {
+    const text = event.currentTarget.value
+    setSearchValue(() => text)
+    if(text!==''){
+      deboucingSearch(text)
+    }else if(text===''){
+      clearTimeout(debounceTimer)
+      setTodoList(()=>INIT_TODO)
+      
+    }
   }
 
   const handleChange = (e) => {
@@ -43,8 +86,27 @@ function TodoList() {
 
   return (
     <div className={styles.todoList}>
+      <div 
+        className={
+          `${styles.searchBox} 
+          ${isSearchClicked ? styles.clicked : ''}`
+        }
+      >
+        <input 
+          className={ `${styles.searchInput} 
+            ${isSearchClicked ? styles.clicked : ''}`
+          }
+          type='text'  
+          value={searchValue}
+          onChange={handleSearchChange}
+          placeholder='search'
+          disabled={!isSearchClicked}
+        />
+      </div>
+
+      <Magnify className={styles.magnify} onClick={handleSearchClick}/>
       <div className={styles.centering}>
-        <h1>Hi! this is your assignment.</h1>
+        <h1 className={styles.greetings}>Hi! this is your assignment.</h1>
         <ul className={styles.tasks}>
           <p className={styles.tasksTitle}>Today&apos;s</p>
           {todoList.map((todo) => (
@@ -54,11 +116,11 @@ function TodoList() {
                 <CheckIcon />
               </div>
               <p className={`${styles.title} ${todo.done?styles.done:''}`}>{todo.title}</p>
-              
+                              
             </li>
           ))}
         </ul>
-        <button type='button' className={styles.addButton} onClick={handleAddClick} aria-label='Add button' />
+        <button type='button' className={styles.addButton} aria-label='Add button' />
       </div>
     </div>
   )
