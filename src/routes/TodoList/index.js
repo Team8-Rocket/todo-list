1cbbbe25e8e4b9e574ff83a050836492255151df
lumpenop <swk5373@gmail.com>
2022-05-06 20:07:51 +0900

    Feat: Add D-day Fix: Search 버그 수정 1. D-day Date 객체를 이용해서 deadLine - new Date() 차이로 D-day를 구현하였습니다 2. 검색 기능에서 디바운싱을 위한 setTimeout에 의해 검색 창이 닫혀도 전체 todoList 를 불러오지 못하는 부분을 수정하였습니다 - clearTimeout

--- src/routes/TodoList/index.js
+++ src/routes/TodoList/index.js
@@ -1,37 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styles from './TodoList.module.scss'
 import { CheckIcon, Magnify } from '../../assets/svgs'
-import {cx} from '../../styles'
+// import {cx} from '../../styles'
+import classNames from 'classnames'
 
 const INIT_TODO = [
   {
     id: 1,
     title: '계란 2판 사기',
     done: false,
+    deadLine: '2022-05-04',
   },
   {
     id: 2,
     title: '맥북 프로 M1 Max CTO 버전 사기',
     done: false,
+    deadLine: '2022-06-08',
   },
   {
     id: 3,
     title: '오늘의 TIL 작성하기',
     done: false,
+    deadLine: '2023-07-08',
   },
 ]
 
-
-
 function TodoList() {
+  const [today, setToday] = useState(new Date())
   const [todoList, setTodoList] = useState(INIT_TODO)
   const [isSearchClicked, setIsSearchClicked] = useState(false)
   const [searchValue, setSearchValue] = useState('')
   const [debounceTimer, setDebounceTimer] = useState(0)
+  
+
+  useEffect(()=>{
+    const now = new Date()
+      
+    setToday(() => now)
+  },[todoList])
+
+  const getDDay = (deadLine) =>{
+    const goal = new Date(`${deadLine}T23:59:00+0900`)
+    const difference = goal - today
+    const dDay = Math.floor(difference / (1000*60*60*24))
+    return dDay
+  }
 
 
   const search = (value) =>{
-
     const newTodo = INIT_TODO.filter(ele => {
       const title = ele.title.replace(' ','').toLowerCase()
       const newValue = value.replace(' ','')[0].toLowerCase()
@@ -48,16 +64,15 @@ function TodoList() {
     }
     const newTimer = setTimeout(async () =>{
       await search(text)
-    }, 250)
+    }, 200)
     setDebounceTimer(newTimer)
   }
 
   const handleSearchClick = () => {
+    clearTimeout(debounceTimer)
     setSearchValue(()=>'')
     setIsSearchClicked(prev => !prev)
-    if(isSearchClicked){
-      setTodoList(()=>INIT_TODO)
-    }
+    setTodoList(()=>INIT_TODO)
   }
 
   const handleSearchChange = (event) => {
@@ -68,7 +83,6 @@ function TodoList() {
     }else if(text===''){
       clearTimeout(debounceTimer)
       setTodoList(()=>INIT_TODO)
-      
     }
   }
 
@@ -109,16 +123,26 @@ function TodoList() {
         <h1 className={styles.greetings}>Hi! this is your assignment.</h1>
         <ul className={styles.tasks}>
           <p className={styles.tasksTitle}>Today&apos;s</p>
-          {todoList.map((todo) => (
-            <li key={`todo-${todo.id}`} className={styles.task}>
-              <div className={styles.checkboxWrapper}>
-                <input type='checkbox' checked={todo.done} data-id={todo.id} onChange={handleChange} />
-                <CheckIcon />
-              </div>
-              <p className={`${styles.title} ${todo.done?styles.done:''}`}>{todo.title}</p>
-                              
-            </li>
-          ))}
+          {todoList.map((todo) => {
+            let { deadLine } = todo
+          
+            const day = getDDay(deadLine)
+            return (
+              <li key={`todo-${todo.id}`} className={styles.task} >
+                <div className={styles.checkboxWrapper} >
+                  <input type='checkbox' checked={todo.done} data-id={todo.id} onChange={handleChange} />
+                  <CheckIcon />
+                </div>
+                <p className={classNames(styles.title, {[styles.done]:todo.done})}
+                >{todo.title}</p>
+                <span 
+                            
+                  className={classNames(styles.dDay, {[styles.dayRed]:day < 3})}
+                >{day>0?`D-${day}`:`D+${Math.abs(day)}`}
+                </span>
+              </li>
+            )
+          })}
         </ul>
         <button type='button' className={styles.addButton} aria-label='Add button' />
       </div>
